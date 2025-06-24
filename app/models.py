321f2cb0ef8590c6b46b6a0fa95f7c4af053bfe9
noanoach <noanoach11@gmail.com>
2025-06-24 14:35:37 +0300

    connect database

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     last_name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     phone_number = db.Column(db.String(20))
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
     token_version = db.Column(db.Integer, default=0)
     permission_type = db.Column(db.String(20), nullable=False, default="user")
     preferredLanguages = db.Column(db.String(100), nullable=True)
